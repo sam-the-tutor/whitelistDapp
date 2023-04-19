7579769c7c98c90e45b556f9a0605ac8cfeead39
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-04-19 16:23:10 +0300

    g

--- src/whitelistDapp_frontend/src/index.js
+++ src/whitelistDapp_frontend/src/index.js
@@ -1,19 +1,81 @@
-import { whitelistDapp_backend } from "../../declarations/whitelistDapp_backend";
+import { whitelistDapp_backend ,canisterId, createActor} from "../../declarations/whitelistDapp_backend";
+import {AuthClient} from "@dfinity/auth-client";
+import { Principal } from "@dfinity/principal";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+let authenticatedCanister = whitelistDapp_backend;
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await whitelistDapp_backend.greet(name);
+let myPrincipal;
 
-  button.removeAttribute("disabled");
+let authClient;
 
-  document.getElementById("greeting").innerText = greeting;
+let identity;
 
-  return false;
-});
+
+async function userLogin(){
+
+  authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated();
+  } else {
+    await authClient.login({
+      identityProvider: process.env.II_URL,
+      onSuccess: () => {
+        handleAuthenticated();
+      },
+    });
+  }
+};
+
+async function handleAuthenticated() {
+  identity = await authClient.getIdentity();
+  myPrincipal = await identity._principal.toString();
+
+  // console.log("canister Id:",canisterId)
+  // console.log("myPrincipal : ",myPrincipal);
+  //  console.log("AuthClient : ",authClient);
+
+  authenticatedCanister  = createActor(canisterId, {
+       agentOptions:{
+            identity,
+         },
+       
+     });
+
+
+  document.getElementById("greeting").innerText = `Your Local Internet Identity Principal is :${myPrincipal}`;
+
+  
+}
+
+
+//prompt login when page is loaded
+window.addEventListener('load', async ()=>{
+  await userLogin()
+})
+
+
+
+
+
+
+
+// document.querySelector("form").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const button = e.target.querySelector("button");
+
+//   const name = document.getElementById("name").value.toString();
+
+//   button.setAttribute("disabled", true);
+
+//   // Interact with foo actor, calling the greet method
+//   const greeting = await whitelistDapp_backend.greet(name);
+
+//   button.removeAttribute("disabled");
+
+//   document.getElementById("greeting").innerText = greeting;
+
+//   return false;
+// });
